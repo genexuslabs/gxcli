1c7c46eaa986d9879bf22a663d316189b6a0c91c
Seba Gomez <sebastiangomezcorrea@gmail.com>
2020-05-16 12:39:29 -0300

    Import/Export verbs implemented

---
+++ src/modules/XPZ/ExportProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using common;
+using gxcli.common;
+
+namespace XPZ
+{
+	public class ExportProvider : IGXCliVerbProvider
+	{
+		public string Name => "Export";
+
+		public string Description => "Exports selected objects from the Knowledge Base to a file.";
+
+		public string Target => "Export";
+
+		public List<VerbParameter> Parameters => new List<VerbParameter>(KBBasedVerbProvider.KBParameters)
+		{
+			new VerbParameter { Name = "FilePath", Description = "Full path to the file to import.", Required = true },
+			new VerbParameter { Name = "ObjectNames", Description = "Objects to be exported.", Required = true}
+		};
+
+		public List<Example> Examples => new List<Example>
+		{
+		};
+	}
+}
---
+++ src/modules/XPZ/ImportProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using common;
+using gxcli.common;
+
+namespace XPZ
+{
+	public class ImportProvider : IGXCliVerbProvider
+	{
+		public string Name => "Import";
+
+		public string Description => "Imports an export file into the Knowledge Base.";
+
+		public string Target => "Import";
+
+		public List<VerbParameter> Parameters => new List<VerbParameter>(KBBasedVerbProvider.KBParameters)
+		{
+			new VerbParameter { Name = "FilePath", Description = "Full path to the file to import.", Required = true }
+		};
+
+		public List<Example> Examples => new List<Example>
+		{
+		};
+	}
+}
